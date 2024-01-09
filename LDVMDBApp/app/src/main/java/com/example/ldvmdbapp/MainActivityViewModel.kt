5780ef339045eef7dcee2a1c7e1573d30a34a23e
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-09 17:05:19 +0000

    Live Data + View Model + DB

---
+++ LDVMDBApp/app/src/main/java/com/example/ldvmdbapp/MainActivityViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.ldvmdbapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    var counter = MutableLiveData<Int>()
+    val countData: LiveData<Int>
+        get() = counter
+
+    init{
+        counter.value = 0
+    }
+
+    fun updateCounter(){
+        counter.value = (counter.value)?.plus(1)
+    }
+
+}
