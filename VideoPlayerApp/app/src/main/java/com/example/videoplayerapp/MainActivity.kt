99b1330d4f9685b928be83ea2e7c67fb29b3d2da
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-09 16:28:20 +0000

    Video Player App

---
+++ VideoPlayerApp/app/src/main/java/com/example/videoplayerapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.videoplayerapp
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Display Local Video
+        val videoView : VideoView = findViewById(R.id.videoview1)
+        videoView.setVideoPath("android.resource://"
+                                +packageName+"/"
+                                +R.raw.mountains)
+
+
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start()
+
+
+        //Display videos from Internet
+        val videoView2 : VideoView = findViewById(R.id.videoview2)
+        val uri = Uri.parse("https://static.videezy.com/system/resources/previews/000/002/231/original/5226496.mp4")
+
+        videoView2.setVideoURI(uri)
+
+        //MediaController
+        val mediaController2 = MediaController(this)
+        mediaController2.setAnchorView(videoView2)
+        videoView2.setMediaController(mediaController2)
+        videoView2.start()
+
+
+
+    }
+}
