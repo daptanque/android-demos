104a44e4d80e84bdc3924c2b97e7195a263530a5
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-09 16:47:10 +0000

    View Pager App

---
+++ ViewPagerApp/app/src/main/java/com/example/viewpagerapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.viewpagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.adapter.MyAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var myAdapter: MyAdapter
+    lateinit var viewPager: ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        myAdapter = MyAdapter(this)
+
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        viewPager.adapter = myAdapter
+
+
+    }
+}
